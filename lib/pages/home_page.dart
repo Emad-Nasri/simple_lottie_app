1f496f5a176bf5cdda5ea28b5b61e95547d530bf
Emad-Nasri <emadnasri50@gmail.com>
2026-02-10 02:54:42 +0300

    showed lottie in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: _buildUI());
+  }
+
+  Widget _buildUI() {
+    return Stack(
+      children: [
+        Center(
+          child: Lottie.asset(
+            'assets/animations/blue loading.json',
+            repeat: true,
+            reverse: true,
+            width: 250,
+            height: 250,
+          ),
+        ),
+        Lottie.asset(
+          'assets/animations/Confetti.json',
+          width: MediaQuery.sizeOf(context).height,
+          height: MediaQuery.sizeOf(context).width,
+          fit: BoxFit.cover,
+          repeat: false,
+        ),
+      ],
+    );
   }
 }
