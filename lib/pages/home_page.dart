59a7e869fa02097b4c39f54698726d3d8b120a25
Emad-Nasri <emadnasri50@gmail.com>
2026-02-10 03:11:54 +0300

    added initState and dispose to control in _controler

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,10 +8,38 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Durations.extralong4,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildUI());
+    return Scaffold(
+      body: _buildUI(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          var ticker = _controller.forward();
+          ticker.whenComplete(() {
+            _controller.reset();
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
   }
 
   Widget _buildUI() {
@@ -21,13 +49,14 @@ class _HomePageState extends State<HomePage> {
           child: Lottie.asset(
             'assets/animations/blue loading.json',
             repeat: true,
-            reverse: true,
+            //reverse: true,
             width: 250,
             height: 250,
           ),
         ),
         Lottie.asset(
-          'assets/animations/Confetti.json',
+          'assets/animations/Confetti (1).json',
+          controller: _controller,
           width: MediaQuery.sizeOf(context).height,
           height: MediaQuery.sizeOf(context).width,
           fit: BoxFit.cover,
